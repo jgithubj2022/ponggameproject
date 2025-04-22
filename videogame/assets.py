2c2357af5a2ba7c594f8f11568a659c931a09522
Jiles Smith <jjilescoll2022ful@csu.fullerton.edu>
2025-04-22 11:06:28 -0700

    videogame changes

--- videogame/assets.py
+++ videogame/assets.py
@@ -1,3 +1,14 @@
+"""Game assets and where they are located. This is a lazy/OK solution 
+to creating a singleton class."""
+
+from os import path
+from videogame.pong_asset_dict import pong_asset_dict as asset_dict
+
+main_dir = path.split(path.abspath(__file__))[0]
+data_dir = path.join(main_dir, "data")
+
+
+
 def get(key):
     """Given the key representing the asset, return a fully qualified
     path to the asset."""
